2e75734cb5bebe04e3c384616a98a665e473a6fe
Ryan Rayfield <ryan.rayfield@ussquash.com>
2015-11-21 10:02:42 -0500

    added project section

--- js/helper.js
+++ js/helper.js
@@ -13,6 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
+
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
@@ -85,7 +86,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+ var x,y;
+ x = loc.pageX;
+ y = loc.pageY;
+
+ logClicks(x,y);
 });
 
 
@@ -142,6 +147,16 @@ function initializeMap() {
 
     return locations;
   }
+  
+//  function locationizer(work_obj){
+   // var locationArray = [];
+    
+  //  for (var job in work_obj.jobs) {
+  //    var newLocation = work_obj.jobs[job].location;
+  //    locationArray.push(newLocation)
+  //  }
+  // return locationArray;
+  //}
 
   /*
   createMapMarker(placeData) reads Google Places search results to create map pins.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,191 @@
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*
+$("#main").append("Ryan Rayfield");
+var awesomeThoughts = "I am Ryan and I am AWESOME";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "fun");
+
+console.log(funThoughts);
+$("#main").append(funThoughts);
+*/
+var formattedName = HTMLheaderName.replace("%data%", "Ryan Rayfield");
+$("#header").prepend(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "CTO");
+$("#header").append(formattedRole);
+
+//var skills = ["VB Script", "T-SQL", "html", "css", "Javascript"];
+
+
+//$("#main").append(bio.welcomeMessage);
+
+
+var work = {
+	"jobs":[{
+		"employer":"Accenture",
+		"title": "Analyst",
+		"location": "Philadelphia",
+		"dates": "2007-2009",
+		"description": "Power Point Monkey",
+		"images": ["url1", "url2","url3"]
+	}, {
+		"employer":"Proscape",
+		"title": "Reporting and Anlytics lead",
+		"location": "Philadelphia",
+		"dates": "2009-2012",
+		"description": "Led reporting team",
+		"images": ["url1", "url2","url3"]
+		}
+		,{
+		"employer":"US Squash",
+		"title": "Director of Technology",
+		"location": "Philadelphia",
+		"dates": "2012-present",
+		"description": "Head of product and leader of engineering teams",
+		"images": ["url1", "url2","url3"]
+		}
+	]
+};
+
+var projects = {
+	"projects": [{
+	"title": "Portfolio",
+	"dates": "Oct",
+	"description": "Great Portfolio",
+	"images":"http://placeimg.com/640/480/arch"
+	}
+	, {
+	"title": "Resume",
+	"dates": "Nov",
+	"description": "Great Resume",
+	"images":"http://placeimg.com/640/480/arch"
+		
+	}]
+};
+
+projects.display = function() {
+	for (project in  projects.projects){
+		$("#projects").append(HTMLprojectStart);
+		console.log(projects.projects[project]);
+		var fomattedTitle=HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$("#projects").append(fomattedTitle);
+		var fomattedDate=HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$("#projects").append(fomattedDate);
+		var fomattedDescription=HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$("#projects").append(fomattedDescription);
+		var fomattedImage=HTMLprojectImage.replace("%data%", projects.projects[project].images);
+		$("#projects").append(fomattedImage);
+	}
+}
+
+projects.display();
+
+var bio = {
+	"name": "Ryan Rayfield",
+	"role": "CTO",
+	"contacts": {
+		"mobile": "609-555-5555",
+		"email": "r***@gmail.com",
+		"location": "Philadelphia", 
+		"github" : "ryanrayfield", 
+		"twitter": "koaray"
+	},
+	"biopic": "images/ryan.jpg",
+	"welcomeMessage": "A man with a plan",
+	"skills": ["VB Script", "T-SQL", "html", "css", "Javascript"]
+};
+
+
+var education = {
+	"schools" : [
+		{
+		"name":"University of Pennsylvania",
+		"location": "Philadeliphia",
+		"degree": "BA",
+		"majors": ["Material Science", "Mathematics"]
+		},
+		{
+		"name": "Udacity",
+		"location": "San Fran",
+		"degree": "Nano",
+		"majors": ["Front End Developer", "Full Stack Engineer"]
+		}
+	], 
+	"onlineCourses":[{
+		"title": "Into to HTML",
+		"school": "Udacity",
+		"dates": "October - December 2015",
+		"url":"www.udacity.com"
+	},	{
+		"title": "Intro to Electrical Engineering",
+		"school": "MIT",
+		"dates": "October - December 2013",
+		"url":"www.edx.org"
+		
+	}]
+};
+
+
+if (bio.skills.length >0 ){
+	
+$("#header").append(HTMLskillsStart);
+
+var formattedSkill= HTMLskills.replace("%data%", bio.skills[0]);
+$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+$("#skills").append(formattedSkill);
+};
+
+
+function displaywork(){
+	$("#workExperience").append(HTMLworkStart);
+	
+for (job in work.jobs){
+	console.log(work.jobs[job]);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+};
+}
+displaywork();
+
+
+
+function inName(name){
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1]= name[1].toUpperCase();
+	name[0]=name[0].slice(0,1).toUpperCase() +
+	name[0].slice(1).toLowerCase();
+	
+	
+	return 	name[0]+" "+name[1];
+	/*
+	var fullName = name.trim().split(" ");
+	console.log(fullName);
+	var firstIn=fullName[0].slice(0,1).toUpperCase();
+	firstIn.slice(1).toLowerCase();
+	var lastIn = fullName[1].toUpperCase();
+	var internationalName = firstIn+" "+lastIn;
+	return internationalName;
+	*/
+};
+$('#main').append(internationalizeButton);
+//inName(bio.name);
